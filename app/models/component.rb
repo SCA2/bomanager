84df081840cea3391be53dc1dc20e3cff1253b0d
SCA2 <tpryan_1965@yahoo.com>
2015-07-04 11:28:32 -0700

    environment variables

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'net/http'
+
 class Component < ActiveRecord::Base
   belongs_to  :category
   has_many    :properties
@@ -13,7 +16,30 @@ class Component < ActiveRecord::Base
   end
 
   def price
-    0.25
-  end
 
+    url = "http://octopart.com/api/v3/parts/search?apikey="
+    url += ENV["OCTOPART_API_KEY"]
+    
+    def to_query(hsh)
+      hsh.map {|k, v| "#{k}=#{URI.encode(v.to_s)}"}.join("&")
+    end
+
+    params = {
+      q: name,
+      start: 0,
+      limit: 10,
+      # sortby: "offers.prices.USD asc, offers.in_stock_quantity desc"
+    }
+
+    url += "&" + to_query(params)
+
+    resp = Net::HTTP.get_response(URI.parse(url))
+    search_response = JSON.parse(resp.body)
+
+    if search_response['results'][0]['item']['offers'][0]['prices'] != {}
+      search_response['results'][0]['item']['offers'][0]['prices']['USD'][0][1].to_f
+    else
+      0
+    end
+  end
 end
